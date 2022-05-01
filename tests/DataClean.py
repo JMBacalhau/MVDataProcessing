4991c67d746446b27006e717175635aec789cdd6
João Bacalhau <joaomarcusbacalhau@gmail.com>
2022-04-30 21:07:19 -0300

    Update DataClean.py

--- tests/DataClean.py
+++ tests/DataClean.py
@@ -5,10 +5,10 @@ Created on Mon Apr 18 06:45:44 2022
 @author: Bacalhau
 """
 """
-DataClean
-Pré Interpolação
-proporção de fases
-pós interpolação
+Remove outliers
+RemoveOutliersHistoGram
+Proporção entre fases
+Input
 
 """
 
@@ -18,22 +18,28 @@ import datetime as dt
 import numpy as np
 from datetime import datetime
 from itertools import combinations
+from scipy import stats
  
-pd.set_option('display.max_rows', None)
-pd.set_option('display.max_columns', None)
-pd.set_option('display.width', None)
-pd.set_option('display.max_colwidth', None)
+#pd.set_option('display.max_rows', None)
+#pd.set_option('display.max_columns', None)
+#pd.set_option('display.width', None)
+#pd.set_option('display.max_colwidth', None)
 
-def DataClean(X_Original,start_date_dt,end_date_dt,sample_freq = 5):    
+def DataClean(x_in,start_date_dt,end_date_dt,sample_freq = 5,sample_time_base = 'm'):    
     
+    #np.timedelta64 -> (D)ay, (M)onth, (Y)ear, (h)ours, (m)inutes, or (s)econds.
+    #x_in = dummy.copy(deep=True)
     
+    added_dic = {'s':'ms','m':'s','h':'m','D':'h','M':'D','Y':'M'}
+    floor_dic = {'s':'S','m':'T','h':'H','D':'D','M':'M','Y':'Y'}    
+        
     #----------------------------------------------------------------#
     #Creates a base vector that conntainXs all the samples between data_inicio and data_final filled timestamp and with nan
     
-    qty_data = len(X_Original.columns)
+    qty_data = len(x_in.columns)
                
-    timearray = np.arange(start_date_dt, end_date_dt,np.timedelta64(sample_freq,'m'), dtype='datetime64')
-    timearray = timearray + np.timedelta64(1,'s') # ADD a second to the end so during the sort this samples will be at last (HH:MM:01)
+    timearray = np.arange(start_date_dt, end_date_dt,np.timedelta64(sample_freq,sample_time_base), dtype='datetime64')
+    timearray = timearray + np.timedelta64(1,added_dic[sample_time_base]) # ADD a second/Minute/Hour/Day/Month to the end so during the sort this samples will be at last (HH:MM:01)
     
     vet_amostras = pd.DataFrame(index=timearray,columns=range(qty_data), dtype=object)
     vet_amostras.index.name = 'timestamp'
@@ -43,20 +49,20 @@ def DataClean(X_Original,start_date_dt,end_date_dt,sample_freq = 5):
     #Creates the output dataframe which is the same but witohut the added second.
        
     Y = vet_amostras.copy(deep=True)    
-    Y.index = Y.index.floor("T")#Flush the seconds    
+    Y.index = Y.index.floor(floor_dic[sample_time_base])#Flush the seconds    
     
     #----------------------------------------------------------------#
     #Saves the name of the columns
-    save_columns_name = X_Original.columns.values    
+    save_columns_name = x_in.columns.values    
     
     #----------------------------------------------------------------#
     #Start to process each column
     
-    fase_list = np.arange(0,X_Original.shape[1])   
+    fase_list = np.arange(0,x_in.shape[1])   
     
     for fase in fase_list:                
         
-        X = X_Original.copy(deep=True)
+        X = x_in.copy(deep=True)
         X.columns = Y.columns
         X = X.loc[~X.iloc[:,fase].isnull(),fase]#Gets only samples on the phase of interest                
         X = X[np.logical_and(X.index<end_date_dt,X.index>=start_date_dt)]#Get samples on between the start and end of the period of study
@@ -69,7 +75,7 @@ def DataClean(X_Original,start_date_dt,end_date_dt,sample_freq = 5):
                         
             #remove seconds (00:00:00) to put this specific samples at the beginning during sort
             df_X = df_X.sort_index(ascending=True)#Ensures the sequence of timestamps
-            df_X.index = df_X.index.round('1min')#Remove seconds, rounding to the nearest minute
+            df_X.index = df_X.index.round('1'+floor_dic[sample_time_base])#Remove seconds, rounding to the nearest minute
             df_X = df_X[df_X.index.minute % sample_freq == 0]#Samples that are multiple of sample_freq have preference 
             
             if(df_X.empty != True):
@@ -81,7 +87,7 @@ def DataClean(X_Original,start_date_dt,end_date_dt,sample_freq = 5):
                 df_aux = df_aux.sort_index(ascending=True)#Ensures the sequence of timestamps    
                 
                 #Elimina segundos (00:00:00), e elimina duplicatas deixando o X quando existente e vet_amostras quando não existe a amostra
-                df_aux.index = df_aux.index.floor("T")
+                df_aux.index = df_aux.index.floor(floor_dic[sample_time_base])
                 df_aux = df_aux[~df_aux.index.duplicated(keep='first')]#Remove unecessary duplicates     
                 
                 #Make sure that any round up that ended up out of the period of study is removed
@@ -97,14 +103,14 @@ def DataClean(X_Original,start_date_dt,end_date_dt,sample_freq = 5):
                             
             #remove seconds (00:00:00) to put this specific samples at the beginning during sort
             df_X = df_X.sort_index(ascending=True)#Ensures the sequence of timestamps
-            df_X.index = df_X.index.round('1min')#Remove seconds, rounding to the nearest minute
+            df_X.index = df_X.index.round('1'+floor_dic[sample_time_base])#Remove seconds, rounding to the nearest minute
             df_X = df_X[df_X.index.minute % sample_freq != 0]#Samples that are NOT multiple of sample_freq have preference 
                             
             
             if(df_X.empty != True):
                
                                                
-                df_X.index = df_X.index.round(str(sample_freq)+'min')#Aproximate sample to the closest multiple of sample_freq
+                df_X.index = df_X.index.round(str(sample_freq)+floor_dic[sample_time_base])#Aproximate sample to the closest multiple of sample_freq
                 
                 df_X = df_X[~df_X.index.duplicated(keep='first')]#Remove unecessary duplicates     
                
@@ -113,7 +119,7 @@ def DataClean(X_Original,start_date_dt,end_date_dt,sample_freq = 5):
                 df_aux = df_aux.sort_index(ascending=True)#Ensures the sequence of timestamps                
                 
                 #Remove seconds (00:00:00), and remove ducplicates leaving X when there is data and vet amostra when its empty
-                df_aux.index = df_aux.index.floor("T")
+                df_aux.index = df_aux.index.floor(floor_dic[sample_time_base])
                 df_aux = df_aux[~df_aux.index.duplicated(keep='first')]#Remove unecessary duplicates     
                 
                 
@@ -132,7 +138,6 @@ def DataClean(X_Original,start_date_dt,end_date_dt,sample_freq = 5):
     
     return Y
 
-
 def IntegrateHour(Y,sample_freq = 5):
     
     time_vet_stamp = Y.index[np.arange(0,len(Y.index),int(60/sample_freq))]    
@@ -142,7 +147,6 @@ def IntegrateHour(Y,sample_freq = 5):
     Y.set_index('timestamp', inplace=True)
     
     return Y
-
 
 def Correlation(X):
     
@@ -205,50 +209,135 @@ def PhaseProportonInput(X,sample_freq = 5,threshold_accept = 0.75):
     
     return Y
 
-def GetWeekDayCurve(X,sample_freq = 5,threshold_accept = 1.0):
+def ReturnOnlyValidDays(x_in,sample_freq = 5,threshold_accept = 1.0,sample_time_base = 'm'):
     
-    # X = output.copy(deep=True)
+    X = x_in.copy(deep=True)
     
-    df_count = X.groupby([X.index.year,X.index.month,X.index.day]).count()/(24*60/sample_freq)    
+    qty_sample_dic = {'s':24*60*60,'m':24*60,'h':24}
+    
+    if sample_time_base in ['s','m','h']: 
+        
+        df_count = X.groupby([X.index.year,X.index.month,X.index.day]).count()/(qty_sample_dic[sample_time_base]/sample_freq)    
+        time_vet_stamp = X.index[np.arange(0,len(X.index),int(24*60/sample_freq))]     
+        df_count = df_count.reset_index(drop=True)    
+        df_count.insert(0,'timestamp_day', time_vet_stamp)
+        df_count.set_index('timestamp_day', inplace=True)    
+        df_count = df_count>=threshold_accept    
+        
+        df_count = df_count.sum(axis=1) == df_count.shape[1]
+        df_count.name = 'isValid'
+        df_count = df_count.reset_index()
+        X['timestamp_day'] = X.index.floor("D").values
+        
+        keep_X_index = X.index
+        X = pd.merge(X, df_count,on = 'timestamp_day' ,how ='left')
+        X.index = keep_X_index
+        X = X.loc[X['isValid'] == True,:]
+        
+        X.drop(columns=['isValid','timestamp_day'],inplace=True)
+    else:
+        X = pd.DafaFrame([])
+        df_count = pd.DafaFrame([])
+        
+        
+    return X,df_count
+
+def GetDayMaxMin(x_in,start_date_dt,end_date_dt,sample_freq = 5,threshold_accept = 1.0,exe_param='max'):
+    
+    X = x_in.copy(deep=True)
+    
+    X,_ = ReturnOnlyValidDays(X,sample_freq,threshold_accept)
+        
+    if(exe_param=='max'):
+        Y = X.groupby([X.index.year,X.index.month,X.index.day]).max()
+    else:
+        Y = X.groupby([X.index.year,X.index.month,X.index.day]).min()
+        
     time_vet_stamp = X.index[np.arange(0,len(X.index),int(24*60/sample_freq))]     
-    df_count = df_count.reset_index(drop=True)    
-    df_count.insert(0,'timestamp_day', time_vet_stamp)
-    df_count.set_index('timestamp_day', inplace=True)    
-    df_count = df_count>=threshold_accept    
+    Y = Y.reset_index(drop=True)    
+    Y.insert(0,'timestamp_day', time_vet_stamp)
+    Y.set_index('timestamp_day', inplace=True)    
     
-    df_count = df_count.sum(axis=1) == df_count.shape[1]
-    df_count.name = 'isValid'
-    df_count = df_count.reset_index()
-    X['timestamp_day'] = X.index.floor("D").values
+    Y = DataClean(Y, start_date_dt, end_date_dt,sample_freq = 1,sample_time_base='D')
     
-    keep_X_index = X.index
-    X = pd.merge(X, df_count,on = 'timestamp_day' ,how ='left')
-    X.index = keep_X_index
-    X = X.loc[X['isValid'] == True,:]
+    Y = Y.interpolate(method_type='linear')    
     
-    X.drop(columns=['isValid','timestamp_day'],inplace=True)
-    
-    
-    grouper = X.groupby([X.index.weekday,X.index.hour])
-    
-    maxes = grouper.transform('max')
-    mins = grouper.transform('min')
-    
-    X = (X-mins)/(maxes-mins)
-    
-    X.groupby([X.index.weekday,X.index.hour])
+    return Y
 
+def GetWeekDayCurve(x_in,sample_freq = 5,threshold_accept = 1.0,min_sample_per_day = 3,min_sample_per_workday = 9):
     
-    return 0
-
+    #x_in = dummy.copy(deep=True)
     
+    X = x_in.copy(deep=True)
+    
+    Y,df_count = ReturnOnlyValidDays(X,sample_freq,threshold_accept)
+    
+    #Get valid data statistics
+    df_count = df_count.loc[df_count['isValid'] == True,:]
+    df_stats = df_count.groupby(df_count['timestamp_day'].dt.weekday).count()
+    days_unique = df_stats.shape[0]
+    count_days_unique = df_stats['timestamp_day'].values
+        
+    
+    #Has enough data do use ?
+    if((days_unique==7) and (np.min(count_days_unique)>=min_sample_per_day)):
+        print('Can calculate a curve for every weekday')
+        
+        Y = Y.groupby([Y.index.weekday,Y.index.hour,Y.index.minute]).mean()
+        Y.index.names = ['WeekDay','Hour','Min']    
+        Y = Y.reset_index()
+        
+        #Normalization max min each day
+        grouper = Y.groupby([Y.WeekDay])    
+        maxes = grouper.transform('max')
+        mins = grouper.transform('min')
+        
+        Y.iloc[:,3:] = (Y.iloc[:,3:]-mins.iloc[:,2:])/(maxes.iloc[:,2:]-mins.iloc[:,2:])  
+        
+    else:
+        work_days = df_stats.loc[df_stats.index<=4,'timestamp_day'].sum()
+        sat_qty = df_stats.loc[df_stats.index==5,'timestamp_day'].sum()
+        sun_qty = df_stats.loc[df_stats.index==6,'timestamp_day'].sum()
+        
+        if((work_days>=min_sample_per_workday) and sun_qty>=min_sample_per_day and sat_qty>=min_sample_per_day):
+            print('Can calculate a curve for every weekday and use Sat. and Sun.')
+            
+            Y = Y.groupby([Y.index.weekday,Y.index.hour,Y.index.minute]).mean()
+            Y.index.names = ['WeekDay','Hour','Min']    
+            Y = Y.reset_index()
+            
+            #Normalization max min each day
+            grouper = Y.groupby([Y.WeekDay])    
+            maxes = grouper.transform('max')
+            mins = grouper.transform('min')
+            
+            Y.iloc[:,3:] = (Y.iloc[:,3:]-mins.iloc[:,2:])/(maxes.iloc[:,2:]-mins.iloc[:,2:])  
+            
+            
+            #FALTA PEGAR UM DIA DA SEMANA MAIS PROXIMO PARA COMPLETAR OS INEXISTENTES
+            
+        else:
+            print('Use default curve.')
+            
+            #FALTA ESCREVER UMA DEFAULT E PERMITIR IMPORTAR
+        
+    return Y
+    
+def RemovePeriod(x_in,df_remove):
+    
+    Y = x_in.copy(deep=True)
+    
+    for index,row in df_remove.iterrows():
+        Y.loc[np.logical_and(Y.index>=row[0],Y.index<=row[1]),:] = np.nan
+        
+    return Y
     
 def SimpleProcess(X,start_date_dt,end_date_dt,sample_freq = 5,pre_interpol=False,pos_interpol=False,prop_phases=False,integrate=False,interpol_integrate=False):    
         
     #ORGANIZE->INTERPOLATE->PHASE_PROPORTION->INTERPOLATE->INTEGRATE->INTERPOLATE
     
     #Organize samples
-    Y = DataClean(X,start_date_dt,end_date_dt,sample_freq)
+    Y = DataClean(X,start_date_dt,end_date_dt,sample_freq,sample_time_base='m')
     
     #Interpolate before proportion between phases
     if(pre_interpol!=False):
@@ -269,6 +358,16 @@ def SimpleProcess(X,start_date_dt,end_date_dt,sample_freq = 5,pre_interpol=False
     
     return Y
 
+
+def RemoveOutliersHistoGram(x_in,hard_max=False,hard_min=False,df_avoid_periods = False):
+    
+    return 0 
+
+
+def RemoveOutliersMMDM(x_in,hard_max=False,hard_min=False):
+    
+    return 0 
+
 if __name__ == "__main__":
     
     import time
@@ -276,8 +375,8 @@ if __name__ == "__main__":
     print("Example:")    
     
     
-    data_inicio='2020-01-01'
-    data_final='2020-01-02'
+    data_inicio='2021-01-01'
+    data_final='2021-06-01'
     
     start_date_dt = dt.datetime(int(data_inicio.split("-")[0]),int(data_inicio.split("-")[1]),int(data_inicio.split("-")[2]))
     end_date_dt = dt.datetime(int(data_final.split("-")[0]),int(data_final.split("-")[1]),int(data_final.split("-")[2]))
@@ -288,7 +387,7 @@ if __name__ == "__main__":
         
     dummy = pd.DataFrame(dummy,columns=['timestamp'])
     
-    dummy['VA'] = dummy['timestamp'].dt.minute
+    dummy['VA'] = dummy['timestamp'].dt.hour
     dummy['VB'] = dummy['timestamp'].dt.minute*2
     dummy['VV'] = dummy['timestamp'].dt.minute*3
     dummy['VN'] = dummy['timestamp'].dt.minute*4
@@ -301,6 +400,7 @@ if __name__ == "__main__":
     
     dummy.set_index('timestamp', inplace=True)
     
+    
     for col in ['VA','VB','VV']:
         dummy.loc[dummy.sample(frac=0.001).index, col] = np.nan
     
@@ -308,13 +408,28 @@ if __name__ == "__main__":
     time_init = time.perf_counter()    
     
     #TESTE
-    #dummy = pd.read_csv('SAFC2BSA.csv',names=['timestamp_aux','VA', 'VB', 'VV'],skiprows=1,parse_dates=True)
-    #dummy.insert(loc=0, column='timestamp', value=pd.to_datetime(dummy.timestamp_aux.astype(str)))
-    #dummy = dummy.drop(columns=['timestamp_aux'])
-    #dummy.set_index('timestamp', inplace=True)
+    dummy = pd.read_csv('SAFC2BSA.csv',names=['timestamp_aux','VA', 'VB', 'VV'],skiprows=1,parse_dates=True)
+    dummy.insert(loc=0, column='timestamp', value=pd.to_datetime(dummy.timestamp_aux.astype(str)))
+    dummy = dummy.drop(columns=['timestamp_aux'])
+    dummy.set_index('timestamp', inplace=True)
+    
+    #TESTE MANOBRAS
+    dummy_manobra = pd.read_csv('BancoManobras.csv',names=['EQ','ALIM1', 'ALIM2', 'data_inicio',"data_final"],skiprows=1,parse_dates=True)
+    dummy_manobra = dummy_manobra.iloc[:,-2:]
+    
+    
+    dummy = DataClean(dummy,start_date_dt,end_date_dt,sample_freq= 5,sample_time_base='m')
+
+
+    #TESTED - OK #dummy = DataClean(dummy,start_date_dt,end_date_dt,sample_freq= 5,sample_time_base='m')
+    #TESTED - OK #output = ReturnOnlyValidDays(dummy,sample_freq = 5,threshold_accept = 1.0,sample_time_base = 'm')
+    #TESTED - OK #output = GetDayMaxMin(dummy,start_date_dt,end_date_dt,sample_freq = 5,threshold_accept = 1.0,exe_param='max')
+    #TESTED - OK #output = RemovePeriod(dummy,dummy_manobra)
+    
+    output = GetWeekDayCurve(dummy,sample_freq = 5,threshold_accept = 1.0)
     
     #output = SimpleProcess(dummy,start_date_dt,end_date_dt,sample_freq = 5,pre_interpol=1,pos_interpol=1,integrate=True,interpol_integrate=1)
-    output = SimpleProcess(dummy,start_date_dt,end_date_dt,sample_freq = 5)
+    #output = SimpleProcess(dummy,start_date_dt,end_date_dt,sample_freq = 5)
     
     print("Time spent: " + str(time.perf_counter()-time_init) )
     

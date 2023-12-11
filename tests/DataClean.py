70ca35519d28d7249e9ef585fb4b085f626b9ee8
João Bacalhau <joaomarcusbacalhau@gmail.com>
2023-12-11 07:12:33 -0300

    created new seasonality for current. And started plot NSSC.

--- tests/DataClean.py
+++ tests/DataClean.py
@@ -17,14 +17,13 @@ import FinishedFunctions as f_remove
 
 if __name__ == "__main__":
 
-    start_date_dt = datetime(2021,1,1)  
-    end_date_dt = datetime(2023,1,1)
+    start_date_dt = datetime(2023,1,1)  
+    end_date_dt = datetime(2024,1,1)
 
-    dummy = f_remove.CurrentDummyData()
-    #dummy.plot(title="Current Input (with outliers [A])")
-    
+    dummy = f_remove.CurrentDummyData(qty_weeks=90)
     dummy.drop(columns = ['IN'],inplace=True)
 
+    
     time_stopper = [['time_init', time.perf_counter()]]
     
     
@@ -33,7 +32,7 @@ if __name__ == "__main__":
     time_stopper.append(['DataSynchronization', time.perf_counter()])
     output.plot(title="DataSynchronization")
    
-    output.iloc[0:12*24*90,2] = numpy.nan
+    output.iloc[12*24*3:12*24*10,2] = numpy.nan
     
     output.plot(title="Missing Phase")
     
@@ -74,19 +73,46 @@ if __name__ == "__main__":
  
 
     
-    output.iloc[12*24*90:12*24*180,:] = numpy.nan
+    output.iloc[12*24*15:12*24*26,:] = numpy.nan
     
     f_remove.CountMissingData(output, show=True)
     time_stopper.append(['Lost all phases', time.perf_counter()])
     output.plot(title="Lost all phases")
 
     # Get day max/min values
-    max_vet,_ = f_remove.GetDayMaxMin(output,start_date_dt,end_date_dt,sample_freq=5,threshold_accept=0.5,exe_param='max')         
-    min_vet,_ = f_remove.GetDayMaxMin(output,start_date_dt,end_date_dt,sample_freq=5,threshold_accept=0.5,exe_param='min') 
+    max_vet,max_vet_idx = f_remove.GetDayMaxMin(output,start_date_dt,end_date_dt,sample_freq=5,threshold_accept=0.5,exe_param='max')         
+    min_vet,min_vet_idx = f_remove.GetDayMaxMin(output,start_date_dt,end_date_dt,sample_freq=5,threshold_accept=0.5,exe_param='min') 
     weekday_curve = f_remove.GetWeekDayCurve(output, sample_freq=5, threshold_accept=0.8,min_sample_per_day=3,min_sample_per_workday=9)
 
+    list_df_print = []
+    for col in max_vet.columns:        
+        df_print = pandas.DataFrame(index=max_vet_idx[col].values, dtype=object)
+        df_print[col+'_max'] = max_vet[col].values        
+        df_print = f_remove.DataSynchronization(df_print, start_date_dt, end_date_dt, sample_freq=5, sample_time_base='m')             
+        list_df_print.append(df_print)
+        
+    
+    for col in min_vet.columns:        
+        df_print = pandas.DataFrame(index=min_vet_idx[col].values, dtype=object)
+        df_print[col+'_min'] = min_vet[col].values        
+        df_print = f_remove.DataSynchronization(df_print, start_date_dt, end_date_dt, sample_freq=5, sample_time_base='m')             
+        list_df_print.append(df_print)
+            
+    df_print = pandas.DataFrame([])
+    for col in list_df_print:  
+        if(df_print.size==0):
+            df_print = col                    
+        else:
+            df_print = pandas.concat((df_print,col),axis=1)
+            
+            
+    
+    ax = output.plot(title = 'Data')            
+    df_print.plot.line(ax=ax,color='red',style='.')    
+    
+    matplotlib.pyplot.show()
+    
 
-    '''
     X_pred = f_remove.GetNSSCPredictedSamples(max_vet, min_vet, weekday_curve,start_date_dt,end_date_dt, sample_freq=5,sample_time_base='m')
     time_stopper.append(['X_pred', time.perf_counter()])
     X_pred.plot(title="X_pred")
@@ -112,4 +138,3 @@ if __name__ == "__main__":
 
     #f_remove.ShowExampleSimpleProcess()
     
-    '''

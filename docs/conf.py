5e8d968e3a72b0c142946060ad46d14510857a7a
João Bacalhau <joaomarcusbacalhau@gmail.com>
2023-12-15 13:58:08 -0300

    read the docs

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 import os
 import sys
 #sys.path.insert(0, os.path.abspath('..'))
-sys.path.insert(0, os.path.join(os.path.dirname((os.path.abspath('.')), 'src', 'models')
+sys.path.insert(0, os.path.join(os.path.dirname((os.path.abspath('..')), 'src', 'models')
 
 
 # Configuration file for the Sphinx documentation builder.
